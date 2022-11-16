c1162221eb6f1a36766ddb5f19cedcb161cd5af6
IamZeke <30021375@student.toiohomai.ac.nz>
2022-11-17 09:04:48 +1300

    Finished Code

--- Program.cs
+++ Program.cs
@@ -15,12 +15,14 @@ namespace ConsoleShop
 		public static void ShopFront()
 		{
 			Console.WriteLine("*******************************************************\n");
-			Console.WriteLine($"\tYour currrent shop balance is ${money}.\n");
-			Console.WriteLine("\tShop category 1			 - Press [1].");
-			Console.WriteLine("\tShop category 2			 - Press [2].");
-			Console.WriteLine("\tShop category 3		  	 - Press [3].");
-			Console.WriteLine("\tAdd funds to Shop Balance	 - Press [4].");
-			Console.WriteLine("\tExit Application            - Press [0].\n");
+			Console.WriteLine("\tWelcome to Zeke's Media House\n");
+			Console.WriteLine("*******************************************************\n");
+			Console.WriteLine($"\tYour currrent shop balance is ${money}\n");
+			Console.WriteLine("\tMusic				- Press [1]");
+			Console.WriteLine("\tMovies				- Press [2]");
+			Console.WriteLine("\tVideo Games			- Press [3]");
+			Console.WriteLine("\tAdd funds to Shop Balance	- Press [4]");
+			Console.WriteLine("\tExit Application		- Press [0]\n");
 			Console.WriteLine("*******************************************************\n");
 			Console.WriteLine("\t↓↓↓↓ Enter Your Option ↓↓↓↓");
 			Console.Write("\t");
@@ -81,38 +83,38 @@ namespace ConsoleShop
 			Console.WriteLine("\tPress any button to continue.");
 			Console.ReadKey();
 			Console.Clear();
-			Console.WriteLine("\t*********************************************************************\n");
+			Console.WriteLine("**************************************************************************************\n");
 			Console.WriteLine($"\tYour currrent shop balance is ${money}.\n");
-			Console.WriteLine("\t************ Category One Products *********************************\n");
-			Console.WriteLine("\tProduct 1 ($10)			- to purchase press [1]");
-			Console.WriteLine("\tProduct 2 ($5)				- to purchase press [2]");
-			Console.WriteLine("\tProduct 3 ($5)				- to purchase press [3]");
-			Console.WriteLine("\tProduct 4 ($10)			- to purchase press [4]");
-			Console.WriteLine("\tReturn to Shop Front		- Press [5]\n");
-			Console.WriteLine("\t********************************************************************\n");
+			Console.WriteLine("************ Music Products **************************************************\n");
+			Console.WriteLine("\tJ Cole | 2014 Forest Hills Drive Album ($24)	- to purchase press [1]");
+			Console.WriteLine("\tDoja Cat | Planet Her Album ($28)		- to purchase press [2]");
+			Console.WriteLine("\tHalsey | Manic Album ($17)			- to purchase press [3]");
+			Console.WriteLine("\tBrent Faiyaz | Fuck the World Album ($22)	- to purchase press [4]");
+			Console.WriteLine("\tReturn to Shop Front				- Press [5]\n");
+			Console.WriteLine("*************************************************************************************\n");
 			Console.WriteLine("\t↓↓↓↓ Enter Your Option ↓↓↓↓");
 			Console.Write("\t");
 
 			switch (int.Parse(Console.ReadLine()))
             {
 				case 1:
-					money -= 10;
-					Console.WriteLine($"\tYou have purchased product 1 for $10 your remaining shop funds is ${money}");
+					money -= 24;
+					Console.WriteLine($"\tYou have purchased J Cole | 2014 Forest Hills Drive Album for $24 your remaining shop funds is ${money}");
 					CategoryOne();
 					break;
 				case 2:
-					money -= 5;
-					Console.WriteLine($"\tYou have purchased product 2 for $5 your remaining shop funds is ${money}");
+					money -= 28;
+					Console.WriteLine($"\tYou have purchased Doja Cat | Planet Her Album for $28 your remaining shop funds is ${money}");
 					CategoryOne();
 					break;
 				case 3:
-					money -= 5;
-					Console.WriteLine($"\tYou have purchased product 3 for $5 your remaining shop funds is ${money}");
+					money -= 17;
+					Console.WriteLine($"\tYou have purchased Halsey | Manic Album for $17 your remaining shop funds is ${money}");
 					CategoryOne();
 					break;
 				case 4:
-					money -= 10;
-					Console.WriteLine($"\tYou have purchased product 4 for $10 your remaining shop funds is ${money}");
+					money -= 22;
+					Console.WriteLine($"\tYou have purchased Brent Faiyaz | Fuck the World Album for $22 your remaining shop funds is ${money}");
 					CategoryOne();
 					break;
 				case 5:
@@ -131,39 +133,42 @@ namespace ConsoleShop
 			Console.WriteLine("\tPress any button to continue.");
 			Console.ReadKey();
 			Console.Clear();
-			Console.WriteLine("\t************ Category One Products *************\n");
-			Console.WriteLine("\tProduct 1 ($10)			- to purchase press [1]");
-			Console.WriteLine("\tProduct 2 ($5)				- to purchase press [2]");
-			Console.WriteLine("\tProduct 3 ($5)				- to purchase press [3]");
-			Console.WriteLine("\tProduct 4 ($10)			- to purchase press [4]");
+			Console.WriteLine("******************************************************************\n");
+			Console.WriteLine($"\tYour currrent shop balance is ${money}.\n");
+			Console.WriteLine("************ Movie Products **************************************\n");
+			Console.WriteLine("\tSAW ($16)			- to purchase press [1]");
+			Console.WriteLine("\tBlack Panther ($24)		- to purchase press [2]");
+			Console.WriteLine("\tSmile ($28)			- to purchase press [3]");
+			Console.WriteLine("\tScream ($18)			- to purchase press [4]");
 			Console.WriteLine("\tReturn to Shop Front		- Press [5]\n");
-			Console.WriteLine("\t************************************************\n");
+			Console.WriteLine("******************************************************************\n");
 			Console.WriteLine("\t↓↓↓↓ Enter Your Option ↓↓↓↓");
 			Console.Write("\t");
 
 			switch (int.Parse(Console.ReadLine()))
 			{
 				case 1:
-					money -= 10;
-					Console.WriteLine($"\tYou have purchased product 1 for $10 your remaining shop funds is ${money}");
+					money -= 16;
+					Console.WriteLine($"\tYou have purchased SAW for $16 your remaining shop funds is ${money}");
 					CategoryOne();
 					break;
 				case 2:
-					money -= 5;
-					Console.WriteLine($"\tYou have purchased product 2 for $5 your remaining shop funds is ${money}");
+					money -= 24;
+					Console.WriteLine($"\tYou have purchased Black Panther for $24 your remaining shop funds is ${money}");
 					CategoryOne();
 					break;
 				case 3:
-					money -= 5;
-					Console.WriteLine($"\tYou have purchased product 3 for $5 your remaining shop funds is ${money}");
+					money -= 28;
+					Console.WriteLine($"\tYou have purchased Smile for $28 your remaining shop funds is ${money}");
 					CategoryOne();
 					break;
 				case 4:
-					money -= 10;
-					Console.WriteLine($"\tYou have purchased product 4 for $10 your remaining shop funds is ${money}");
+					money -= 18;
+					Console.WriteLine($"\tYou have purchased Scream for $18 your remaining shop funds is ${money}");
 					CategoryOne();
 					break;
 				case 5:
+					Console.Clear();
 					ShopFront();
 					break;
 				default:
@@ -178,39 +183,42 @@ namespace ConsoleShop
 			Console.WriteLine("\tPress any button to continue.");
 			Console.ReadKey();
 			Console.Clear();
-			Console.WriteLine("\t************ Category One Products *************\n");
-			Console.WriteLine("\tProduct 1 ($10)			- to purchase press [1]");
-			Console.WriteLine("\tProduct 2 ($5)				- to purchase press [2]");
-			Console.WriteLine("\tProduct 3 ($5)				- to purchase press [3]");
-			Console.WriteLine("\tProduct 4 ($10)			- to purchase press [4]");
-			Console.WriteLine("\tReturn to Shop Front		- Press [5]\n");
-			Console.WriteLine("\t************************************************\n");
+			Console.WriteLine("*********************************************************************************\n");
+			Console.WriteLine($"\tYour currrent shop balance is ${money}.\n");
+			Console.WriteLine("************ Game Products ******************************************************\n");
+			Console.WriteLine("\tCounter-Strike Global-Offensive ($20)		- to purchase press [1]");
+			Console.WriteLine("\tCall Of Duty 4 ($27)				- to purchase press [2]");
+			Console.WriteLine("\tWii Sports ($31)				- to purchase press [3]");
+			Console.WriteLine("\tDiablo III ($29)				- to purchase press [4]");
+			Console.WriteLine("\tReturn to Shop Front				- Press [5]\n");
+			Console.WriteLine("*********************************************************************************\n");
 			Console.WriteLine("\t↓↓↓↓ Enter Your Option ↓↓↓↓\n");
 			Console.Write("\t");
 
 			switch (int.Parse(Console.ReadLine()))
 			{
 				case 1:
-					money -= 10;
-					Console.WriteLine($"\tYou have purchased product 1 for $10 your remaining shop funds is ${money}");
+					money -= 20;
+					Console.WriteLine($"\tYou have purchased Counter-Strike Global-Offensiv for $20 your remaining shop funds is ${money}");
 					CategoryOne();
 					break;
 				case 2:
-					money -= 5;
-					Console.WriteLine($"\tYou have purchased product 2 for $5 your remaining shop funds is ${money}");
+					money -= 27;
+					Console.WriteLine($"\tYou have purchased Call Of Duty 4 for $27 your remaining shop funds is ${money}");
 					CategoryOne();
 					break;
 				case 3:
-					money -= 5;
-					Console.WriteLine($"\tYou have purchased product 3 for $5 your remaining shop funds is ${money}");
+					money -= 31;
+					Console.WriteLine($"\tYou have purchased Wii Sports for $31 your remaining shop funds is ${money}");
 					CategoryOne();
 					break;
 				case 4:
-					money -= 10;
-					Console.WriteLine($"\tYou have purchased product 4 for $10 your remaining shop funds is ${money}");
+					money -= 29;
+					Console.WriteLine($"\tYou have purchased Diablo III for $29 your remaining shop funds is ${money}");
 					CategoryOne();
 					break;
 				case 5:
+					Console.Clear();
 					ShopFront();
 					break;
 				default:
