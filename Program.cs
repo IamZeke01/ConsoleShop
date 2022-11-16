dc3d260f66672dc0716691463b47c007d641428a
IamZeke <30021375@student.toiohomai.ac.nz>
2022-11-17 08:36:54 +1300

    Added more work

--- Program.cs
+++ Program.cs
@@ -10,34 +10,36 @@ namespace ConsoleShop
     {
 		// Global Class Variables
 		public static float money = 100;
+		public static string username;
 
 		public static void ShopFront()
 		{
-			Console.WriteLine("******************************************\n");
+			Console.WriteLine("*******************************************************\n");
 			Console.WriteLine($"\tYour currrent shop balance is ${money}.\n");
 			Console.WriteLine("\tShop category 1			 - Press [1].");
 			Console.WriteLine("\tShop category 2			 - Press [2].");
 			Console.WriteLine("\tShop category 3		  	 - Press [3].");
 			Console.WriteLine("\tAdd funds to Shop Balance	 - Press [4].");
-			Console.WriteLine("\tExit Application            - Press [x].\n")
-			Console.WriteLine("******************************************\n");
-			Console.WriteLine("\tvvvv   Enter your option here   vvvv\n");
+			Console.WriteLine("\tExit Application            - Press [0].\n");
+			Console.WriteLine("*******************************************************\n");
+			Console.WriteLine("\t↓↓↓↓ Enter Your Option ↓↓↓↓");
+			Console.Write("\t");
 
-			switch(Console.ReadLine())
+			switch (Console.ReadLine())
             {
-				case 1:
-					CategoryOne;
+				case "1":
+					CategoryOne();
 					break;
-				case 2:
-					CategoryTwo;
+				case "2":
+					CategoryTwo();
 					break;
-				case 3:
-					CategoryThree;
+				case "3":
+					CategoryThree();
 					break;
-				case 4:
-					AddFunds;
+				case "4":
+					AddFunds();
 					break;
-				case x:
+				case "5":
 					Environment.Exit(0);
 					break;
 				default:
@@ -49,32 +51,49 @@ namespace ConsoleShop
 		}
 		public static void Main()
 		{
+			Console.WriteLine("******************************************\n");
+			Console.WriteLine("\tWhat is your name?\n");
+			Console.WriteLine("******************************************\n");
+			username = Console.ReadLine();
+			Console.Clear();
+			Console.WriteLine("*******************************************************\n");
+			Console.WriteLine("\tWelcome, " + username + "\n");
 			ShopFront();
 			Console.ReadKey();
 		}
 
 		public static void AddFunds()
         {
-			Console.WriteLine("Add Funds vvvv");
-			money = int.Parse(Console.ReadLine() + money);
-			Console.Write();
-        }
+			Console.WriteLine("\tPress any button to continue.");
+			Console.ReadKey();
+			Console.Clear();
+			Console.WriteLine("\tHow much money would you like to add? (without $) ↓↓↓↓");
+			money = int.Parse(Console.ReadLine()) + money;
+			Console.WriteLine($"\tShop Funds available is now {money}");
+			Console.WriteLine("\tPress any button to go back to the menu.");
+			Console.ReadKey();
+			Console.Clear();
+			ShopFront();
+		}
 
 		public static void CategoryOne()
         {
 			Console.WriteLine("\tPress any button to continue.");
 			Console.ReadKey();
 			Console.Clear();
-			Console.WriteLine("\t************ Category One Products *************\n");
+			Console.WriteLine("\t*********************************************************************\n");
+			Console.WriteLine($"\tYour currrent shop balance is ${money}.\n");
+			Console.WriteLine("\t************ Category One Products *********************************\n");
 			Console.WriteLine("\tProduct 1 ($10)			- to purchase press [1]");
 			Console.WriteLine("\tProduct 2 ($5)				- to purchase press [2]");
 			Console.WriteLine("\tProduct 3 ($5)				- to purchase press [3]");
 			Console.WriteLine("\tProduct 4 ($10)			- to purchase press [4]");
 			Console.WriteLine("\tReturn to Shop Front		- Press [5]\n");
-			Console.WriteLine("\t************************************************\n");
-			Console.WriteLine("\tvvvv   Enter your option here   vvvv\n");
+			Console.WriteLine("\t********************************************************************\n");
+			Console.WriteLine("\t↓↓↓↓ Enter Your Option ↓↓↓↓");
+			Console.Write("\t");
 
-			switch (Console.ReadLine())
+			switch (int.Parse(Console.ReadLine()))
             {
 				case 1:
 					money -= 10;
@@ -97,6 +116,7 @@ namespace ConsoleShop
 					CategoryOne();
 					break;
 				case 5:
+					Console.Clear();
 					ShopFront();
 					break;
 				default:
@@ -118,9 +138,10 @@ namespace ConsoleShop
 			Console.WriteLine("\tProduct 4 ($10)			- to purchase press [4]");
 			Console.WriteLine("\tReturn to Shop Front		- Press [5]\n");
 			Console.WriteLine("\t************************************************\n");
-			Console.WriteLine("\tvvvv   Enter your option here   vvvv\n");
+			Console.WriteLine("\t↓↓↓↓ Enter Your Option ↓↓↓↓");
+			Console.Write("\t");
 
-			switch (Console.ReadLine())
+			switch (int.Parse(Console.ReadLine()))
 			{
 				case 1:
 					money -= 10;
@@ -164,9 +185,10 @@ namespace ConsoleShop
 			Console.WriteLine("\tProduct 4 ($10)			- to purchase press [4]");
 			Console.WriteLine("\tReturn to Shop Front		- Press [5]\n");
 			Console.WriteLine("\t************************************************\n");
-			Console.WriteLine("\tvvvv   Enter your option here   vvvv\n");
+			Console.WriteLine("\t↓↓↓↓ Enter Your Option ↓↓↓↓\n");
+			Console.Write("\t");
 
-			switch (Console.ReadLine())
+			switch (int.Parse(Console.ReadLine()))
 			{
 				case 1:
 					money -= 10;
